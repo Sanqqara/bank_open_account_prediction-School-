7e6e98574ebcce6c8424641b1080bd0f77f08dfa
Tim Sankara <chimpy.the.dev@outlook.com>
2021-01-11 23:57:43 +0300

    gett simple prediction

--- pyFiles/test.py
+++ pyFiles/test.py
@@ -14,12 +14,26 @@ def printFF():
 
 @app.route("/api/getPrediction", methods=["POST"])
 def getPrediction():
+    req_data = request.get_json()
+
     # Import data
     train_data = pd.read_csv('Train_v2.csv')
     test_data = pd.read_csv('Test_v2.csv')
 
-    # print('train data shape :', train_data.shape)
-    # print('test data shape :', test_data.shape)
+    test_df = train_data.drop("bank_account", axis=1)
+
+    # get the columns for the users data
+    user_data_columns = test_df.columns
+    user_data = []
+
+    # create DataFrame with correct columns
+    for column in req_data["data"]:
+        user_data.append(column)
+    user_data = [user_data]
+    user_data = pd.DataFrame(user_data, columns=user_data_columns)
+
+
+# ***** As at this point the user_data and test_df DataFrames are the same **********
 
     # Label Encoding
 
@@ -27,7 +41,6 @@ def getPrediction():
     # Convert target label to numerical Data
     le = LabelEncoder()
     train_data['bank_account'] = le.fit_transform(train_data['bank_account'])
-    train_data.head()
 
     # valueEncode = []
     # train_data_copy = train_data.copy()
@@ -43,17 +56,28 @@ def getPrediction():
     #  Conversion
     train_data['year_'] = train_data['year']
     test_data['year_'] = test_data['year']
+    user_data['year_'] = user_data['year']
+
     # Convert the following numerical labels from integer to float
     float_array = train_data[['household_size',
                               'age_of_respondent', 'year_']].values.astype(float)
     float_array = test_data[['household_size',
                              'age_of_respondent', 'year_']].values.astype(float)
+    float_array = user_data[['household_size',
+                             'age_of_respondent', 'year_']].values.astype(float)
+    
+    user_data["household_size"] = user_data["household_size"].astype(float)
+    user_data["age_of_respondent"] = user_data["age_of_respondent"].astype(float)
+    user_data["year_"] = user_data["year_"].astype(float)
 
+    # print(user_data.dtypes)
+    
     # Data preprocessing
     # convert categorical features to numerical features
     # categorical features to be converted by One Hot Encoding
     train_data['country_'] = train_data['country']
     test_data['country_'] = test_data['country']
+    user_data['country_'] = user_data['country']
 
     categ = ['relationship_with_head', 'marital_status',
              'education_level', 'job_type', 'country_']
@@ -61,6 +85,8 @@ def getPrediction():
     train_data = pd.get_dummies(train_data, prefix_sep='_', columns=categ)
 
     test_data = pd.get_dummies(test_data, prefix_sep='_', columns=categ)
+
+    user_data = pd.get_dummies(user_data, prefix_sep='_', columns=categ)
 
     # Labelncoder conversion
     train_data['location_type'] = le.fit_transform(train_data['location_type'])
@@ -75,20 +101,43 @@ def getPrediction():
     test_data['gender_of_respondent'] = le.fit_transform(
         test_data['gender_of_respondent'])
 
+    user_data['location_type'] = le.fit_transform(user_data['location_type'])
+    user_data['cellphone_access'] = le.fit_transform(
+        user_data['cellphone_access'])
+    user_data['gender_of_respondent'] = le.fit_transform(
+        user_data['gender_of_respondent'])
+    
     # Separate training features from target
     X_train = train_data.drop(
         ['year', 'uniqueid', 'bank_account', 'country'], axis=1)
     y_train = train_data['bank_account']
 
     X_test = test_data.drop(['year', 'uniqueid', 'country'], axis=1)
+    user_data = user_data.drop(['year', 'uniqueid', 'country'], axis=1)
 
-    # rescale X_train and X_test
+    # find the columns in the test data and not in the user data
+    columns_not_present = np.setdiff1d(test_data.columns, user_data.columns)
+    print(columns_not_present)
+
+    # create a df of zeros for each missing column
+    not_present_df = pd.DataFrame(0, index=np.arange(
+        len(columns_not_present)), columns=columns_not_present)
+
+    # join the user_df with the zeros df
+    user_data = pd.concat([user_data, not_present_df])
+
+    # dropping the extra unneccessary columns
+    for missing_column in np.setdiff1d(user_data.columns, X_test.columns):
+        user_data.drop(missing_column, axis=1, inplace=True)
+
+    # rescale X_train, X_test and user_data
     # import MinMaxScaler
     from sklearn.preprocessing import MinMaxScaler
 
     scaler = MinMaxScaler(feature_range=(0, 1))
     X_train_rescaled = scaler.fit_transform(X_train)
     X_test_rescaled = scaler.fit_transform(X_test)
+    user_data_rescaled = scaler.fit_transform(user_data)
 
     # Split train_data
     from sklearn.model_selection import train_test_split
@@ -98,18 +147,15 @@ def getPrediction():
 
     #import XGBClassifier
     from xgboost import XGBClassifier
+    from sklearn.linear_model import LogisticRegression
 
     my_model = XGBClassifier()
 
     my_model.fit(X_Train, y_Train)
-    # print("My model2....", my_model)
 
-    predictions = my_model.predict(X_val)
-    print(X_val)
-    predictions_len = len(predictions)
+    # predictions = my_model.predict(X_val)
+    predictions = my_model.predict(user_data)
 
-    # print(predictions)
-    # print(train_data)
     return ({"Prediction": predictions.tolist()})
 
 
